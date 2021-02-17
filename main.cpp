603f480068695a1344183a82e2ec4e1f0929e00e
panda2134 <ljypanda@live.com>
2021-02-17 11:18:40 +0800

    build(cmake): add json lib

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <string>
+#include "include/json/single_include/nlohmann/json.hpp"
+
+using json = nlohmann::json;
 
 int main(void) {
+	json j;
+	std::cin >> j;
+	std::cout << j["replay"];
 	return 0;
 }
