a55fcb688ed4b522c1d40b3f8d9fbe6da881452e
ALin <jl-zhang18@mails.tsinghua.edu.cn>
2021-02-20 16:16:09 +0800

    Initialize box2d

--- world.cpp
+++ world.cpp
@@ -1,18 +1,22 @@
-#include <random>
-#include <cmath>
 #include "world.h"
+#include <cmath>
+#include <random>
 
-const double thuai::RADIUS = 40.0, thuai::GOAL_LENGTH = 10.0, thuai::GOAL_WIDTH = 5.0;
+const double thuai::RADIUS = 40.0, thuai::GOAL_LENGTH = 10.0,
+             thuai::GOAL_WIDTH = 5.0;
 
 thuai::World::World() {
   const double pi = acos(-1);
   {
     const double angle_delta = 2 * pi / (3 * 5), player_radius = RADIUS / 2;
     double angle = .0;
+
     for (int i = 0; i < PLAYER_COUNT; i++) {
-      if (i % 4 == 0) angle += angle_delta;
+      if (i % 4 == 0)
+        angle += angle_delta;
       players[i] = new Player(i);
-      players[i]->set_position({player_radius * cos(angle), player_radius * sin(angle)});
+      players[i]->set_position(
+          {player_radius * cos(angle), player_radius * sin(angle)});
       angle += angle_delta;
     }
   }
@@ -24,9 +28,11 @@ thuai::World::World() {
       const double egg_radius_delta = RADIUS / 6;
       for (int k = 1; k <= 5; k++, egg_id++) {
         double egg_radius = egg_radius_delta * k;
-        double score = mtgen() * 10.0 / std::mt19937::max() + 10.0;       // score: [10, 20)
+        double score =
+            mtgen() * 10.0 / std::mt19937::max() + 10.0;  // score: [10, 20)
         eggs[egg_id] = new Egg(egg_id, score);
-        eggs[egg_id]->set_position({egg_radius * cos(angle), egg_radius * sin(angle)});
+        eggs[egg_id]->set_position(
+            {egg_radius * cos(angle), egg_radius * sin(angle)});
       }
     }
   }
--- world.h
+++ world.h
@@ -1,17 +1,19 @@
 #ifndef WORLD_H
 #define WORLD_H
 
+#include "Box2D/Box2D.h"
 #include "cpp_record_sdk/entities.h"
 
 namespace thuai {
-  const double RADIUS, GOAL_LENGTH, GOAL_WIDTH;
+const double RADIUS = 40.0, GOAL_LENGTH = 10.0, GOAL_WIDTH = 5.0;
+const b2Vec2 gravity(.0, .0);
 
-  struct World {
-    Player* players[PLAYER_COUNT];
-    Egg* eggs[EGG_COUNT];
+struct World {
+  Player* players[PLAYER_COUNT];
+  Egg* eggs[EGG_COUNT];
 
-    World();
-    ~World();
-  };
-}
+  World();
+  ~World();
+};
+}  // namespace thuai
 #endif
